033a004a39b7b516fbb0d853888af0e90987a657
symek <szymon.kapeniak@gmail.com>
2015-01-16 16:19:24 +0100

    Cleanups in render callback functions. Move tiling support and render frame list to own routines. Move forward to bigger changes in that area.

--- hafarm/Houdini.py
+++ hafarm/Houdini.py
@@ -26,7 +26,7 @@ class HbatchFarm(hafarm.HaFarm):
         # or any custom render script (harender atm.)
         self.parms['command']     = str(self.node.parm("command").eval())
 
-        # This is because we do tiling by our own:
+        # This is because we do tiling ourselfs:
         if self.rop.type().name() == 'ifd':
             self.parms['command_arg'] += " --ignore_tiles "
 
@@ -110,16 +110,6 @@ class HbatchFarm(hafarm.HaFarm):
             like renderer command and arguments used to render on farm.
         """
 
-
-        # Prepare rop for IFD generation if required:
-        # FIXME: This shouldn't be here at all:
-        # if self.rop.type().name() == 'ifd':
-        #     old_soho_outputmode = self.rop.parm("soho_outputmode").eval()
-        #     old_soho_diskfile   =  self.rop.parm('soho_diskfile').unexpandedString()
-        #     self.rop.parm("soho_outputmode").set(1)
-        #     path = os.path.join(self.node.parm("ifd_path").eval(), self.parms['job_name'] + ".$F.ifd")
-        #     self.rop.parm('soho_diskfile').set(path)
-
         # Save current state of a scene: 
         # TODO: Make optional:
         # We assume hip file is already saved, otherwise have scenes takes ages to export 
@@ -142,12 +132,6 @@ class HbatchFarm(hafarm.HaFarm):
         # Save to parms again:
         self.parms['command_arg'] = command
 
-        # Restore old ifd file and scene setting:
-        # FIXME: This also shouln't be here at all. 
-        # if self.rop.type().name() == 'ifd':
-        #     self.rop.parm('soho_diskfile').set(old_soho_diskfile)
-        #     self.rop.parm('soho_outputmode').set(old_soho_outputmode)
-
         # Any debugging info [object, outout]:
         return ['pre_schedule', 'render with arguments:' + command]
 
@@ -158,15 +142,18 @@ class HbatchFarm(hafarm.HaFarm):
 class MantraFarm(hafarm.HaFarm):
     def __init__(self, node, rop=None, job_name=None, parent_job_name=None, crop_parms=(1,1,0)):
         super(MantraFarm, self).__init__()
+
         # Keep reference to assigned rop
         self.rop = rop
         self.node = node
         self.parms['command_arg']    = ''
         self.parms['command']        = '$HFS/bin/mantra'
+
         # Mantra jobs' names are either derived from parent job (hscript)
         # or provided by user (to allow of using ifd names for a job.)
         if not job_name: job_name    = parent_job_name
         self.parms['job_name']       = job_name + '_mantra'
+
         # Tiling support:
         if crop_parms != (1,1,0):
             self.parms['job_name']  += "_tile%s" % str(crop_parms[2])
@@ -179,10 +166,13 @@ class MantraFarm(hafarm.HaFarm):
         if self.node.parm("ignore_check").eval():
             self.parms['ignore_check'] = True
 
+        # Mailing support based on SGE, make it more robust. 
         self.parms['email_list']   = [utils.get_email_address()]
         if self.node.parm("add_address").eval():
             self.parms['email_list'] += list(self.node.parm('additional_emails').eval().split())
         self.parms['email_opt']   = str(self.node.parm('email_opt').eval())
+
+        # Queue and group details:
         self.parms['queue']       = str(self.node.parm('queue').eval())
         self.parms['group']       = str(self.node.parm('group').eval())
         self.parms['job_on_hold'] = bool(self.node.parm('job_on_hold').eval())
@@ -196,6 +186,7 @@ class MantraFarm(hafarm.HaFarm):
         # Doesn't make sense for Mantra, but will be expected as usual later on:
         self.parms['frame_range_arg'] = ["%s%s%s", '', '', ''] 
         self.parms['req_resources'] = 'procslots=%s' % int(self.node.parm('slots').eval())
+
         # Hold until parent job isn't completed
         if parent_job_name:
             self.parms['hold_jid'] = [parent_job_name]
@@ -252,51 +243,50 @@ class MantraFarm(hafarm.HaFarm):
 
 
 
-class ICompFarm(hafarm.HaFarm):
-    '''This small utility class calls icomp exec, which is Python wrapper around nuke comand line mode.
-        icomp has a built-in old school SGE support, which we should replace with new stuff soon.'''
-    def __init__(self, command=''):
-        super(ICompFarm, self).__init__()
-        self._exec    = '/STUDIO/scripts/icomp/icomp'
-        self.command = command
+def render_frame_list(node, rop, hscript_farm, frames):
+    """Make a list of individual frames and send separately to manager
+    This basically means HaFarm doesn't support any batching of random set of frames
+    so we manage them individually. Unlike hscript exporter (HBachFarm), which does recognize
+    frame_list parameter and via harender script supporting random frames."""
+
+    mantra_frames = []
+    for frame in frames:
+        mantra_farm = MantraFarm(node, rop, job_name=None, parent_job_name=hscript_farm.parms['job_name'])
+        # Single task job:
+        mantra_farm.parms['start_frame'] = frame
+        mantra_farm.parms['end_frame']   = frame
+        show_details("Mantra", mantra_farm.parms, mantra_farm.render()) 
+        mantra_frames.append(mantra_farm)
+
+    return mantra_frames
 
 
-    def render(self):
-        from os import popen
-        result = popen(self._exec + self.command).readlines()
-        return result
+def render_with_tiles(node, rop, hscript_farm):
+    job_ids = []
+    mantra_tiles = []
+    tiles_x = rop.parm('vm_tile_count_x').eval()
+    tiles_y = rop.parm('vm_tile_count_y').eval()
 
+    for tile in range(tiles_x*tiles_y):
+        mantra_farm = MantraFarm(node, rop, job_name = None, parent_job_name = hscript_farm.parms['job_name'], \
+                                                            crop_parms = (tiles_x,tiles_y,tile))
+        show_details("Mantra", mantra_farm.parms, mantra_farm.render()) 
+        job_ids.append(mantra_farm.parms['job_name'])
+        mantra_tiles.append(mantra_farm)
 
-    def join_tiles(self, job_parent_name, filename, start, end, ntiles):
-        '''Creates a command specificly for merging tiled rendering.'''
-        from ha.path import padding
+    # Tile merging job:
+    print "Disabled tilinig merging atm."
+    # icomp_farm = ICompFarm(parent_job_name=",".join(job_ids))
+    # icomp_farm.join_tiles(mantra_farm.parms['job_name'], \
+    #                       mantra_farm.parms['output_picture'], \
+    #                       mantra_farm.parms['start_frame'],\
+    #                       mantra_farm.parms['end_frame'],\
+    #                       tiles_x*tiles_y)
+    # print icomp_farm.render()
 
-        # Retrive full frame name (without _tile%i)
-        if "_tile" in filename:
-            base, rest = filename.split("_tile")
-            tmp, ext   = os.path.splitext(filename)
-            filename   = base + ext
-        else:
-            base, ext  = os.path.splitext(filename)
-
-        details = padding(filename, format='nuke')
-        base    = os.path.splitext(details[0])[0]
-        reads   = [base + '_tile%s' % str(tile) + ext for tile in range(ntiles)]
-
-        # Reads:
-        command = ' '
-        for read in reads:
-            command += '--Read file=%s:first=%s:last=%s ' % (read, start, end)
-
-        # Mereges:
-        command += '--Merge over,0,1 ' 
-        for read in range(2, len(reads)):
-            command += '--Merge over,%s ' % read
-
-        # Final touch:
-        command += '--Write file=%s ' % details[0]
-        command += '--globals %s,%s,24 --hold %s -f' % (start, end, job_parent_name)
-        self.command = command
+    # Restore original setting of tiling:
+    # rop.parm("vm_tile_render").set(True)
+    return mantra_tiles
 
 
 
@@ -311,6 +301,7 @@ def render_from_ifd(ifds, start, end, node, job_name=None):
     mantra_farm.parms['start_frame'] = node.parm("ifd_range1").eval() #TODO make automatic range detection
     mantra_farm.parms['end_frame']   = node.parm("ifd_range2").eval() #TODO as above
     mantra_farm.parms['scene_file']  = seq_details[0] + '${SGE_TASK_ID}' + '.ifd'
+
     # Detect output image. Uses grep ray_image on first ifd file:
     # TODO: This is potential source of errors without proper checking that file exists
     path, filename = os.path.split(ifds)
@@ -355,71 +346,28 @@ def render_pressed(node):
     inputs = node.inputs()
     for rop in inputs:
         hscript_farm = HbatchFarm(node, rop)
-
-        # Ignore builtin tile rendering here (see bellow)
-        # If vm_tile_render exists...
-        if rop.parm('vm_tile_render'):
-            # and is set True:
-            if rop.parm('vm_tile_render').eval():
-                tile_render = True
-                #rop.parm('vm_tile_render').set(False)
-
-        # Print details:
         show_details('Hscript', hscript_farm.parms, hscript_farm.render())
 
         # Continue the loop in case this wasn't Mantra ROP.
         if rop.type().name() != 'ifd':
             continue
 
-        # ... or follow with Mantra job:
         # Use as usual frame ranges from connected rops to schedule Mantra renders:
         if not node.parm("use_frame_list").eval():
             # TODO: Move tiling inside MantraFarm class...
-            # This a little harder approach than simple changine tile index and re-call MantraFarm
-            # several times, but I don't want to use tiling parm on a node, because it will re-export ifds
-            # which is waste of resources. I do prefer using python filtering for tiling atm.
-            # Tiling:
-            if tile_render:
-                job_ids = []
-                tiles_x = rop.parm('vm_tile_count_x').eval()
-                tiles_y = rop.parm('vm_tile_count_y').eval()
-
-                for tile in range(tiles_x*tiles_y):
-                    mantra_farm = MantraFarm(node, rop, job_name = None, parent_job_name = hscript_farm.parms['job_name'], \
-                                                                        crop_parms = (tiles_x,tiles_y,tile))
-                    show_details("Mantra", mantra_farm.parms, mantra_farm.render()) 
-                    job_ids.append(mantra_farm.parms['job_name'])
-
-                # Tile merging job:
-                icomp_farm = ICompFarm(parent_job_name=",".join(job_ids))
-                icomp_farm.join_tiles(mantra_farm.parms['job_name'], \
-                                      mantra_farm.parms['output_picture'], \
-                                      mantra_farm.parms['start_frame'],\
-                                      mantra_farm.parms['end_frame'],\
-                                      tiles_x*tiles_y)
-                print icomp_farm.render()
-
-                # Restore original setting of tiling:
-                # rop.parm("vm_tile_render").set(True)
+            # Custom tiling:
+            if rop.parm('vm_tile_render').eval():
+                mantra_tiles = render_with_tiles(node, rop, hscript_farm)
             else:
                 # Proceed normally (no tiling required):
                 mantra_farm = MantraFarm(node, rop, job_name = None, parent_job_name = hscript_farm.parms['job_name'],)
                 show_details("Mantra", mantra_farm.parms, mantra_farm.render()) 
 
-
-
         # Render randomly selected frames provided by the user in HaFarm parameter:
         # TODO: Doesn't suppport tiling atm.
         else:
             frames = node.parm("frame_list").eval()
-            # Make a list of individual frames out of it, and send separately to manager
-            # This basically means HaFarm doesn't support any batching of random set of frames
-            # so we manage them individually. Unlike hscript exporter (HBachFarm), which does recognize
-            # frame_list parameter and via harender script, supports random frames.
             frames = utils.parse_frame_list(frames)
-            for frame in frames:
-                mantra_farm = MantraFarm(node, rop, job_name=None, parent_job_name=hscript_farm.parms['job_name'])
-                # Single task job:
-                mantra_farm.parms['start_frame'] = frame
-                mantra_farm.parms['end_frame']   = frame
-                show_details("Mantra", mantra_farm.parms, mantra_farm.render()) 
+            print frames
+            #mantra_frames = render_frame_list(node, rop, hscript_farm, frames)
+            
