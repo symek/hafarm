e71ef6cfe61498412aceda716da37a92f7752535
Szymon Kapeniak <szymon.kapeniak@gmail.com>
2015-03-19 17:21:29 +0100

    Removed unsed stuff

--- exchange/utils.py
+++
@@ -1,9 +0,0 @@
-
-
-def build_operators_list(module):
-	operators = {}
-	for clsname in dir(module):
-		cls = getattr(module, clsname)
-		if hasattr(cls, 'kind') and clsname != 'ExObjectBase':
-			operators[cls().kind] = clsname
-	return operators
