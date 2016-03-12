84b62f535c90ec4eb8d2315fc7f5e504609900d2
Szymon Kapeniak <szymon.kapeniak@gmail.com>
2016-03-12 21:39:14 +0100

    ConfigParser as a backbone for hafarm config.

--- config.py
+++ config.py
@@ -1,10 +1,20 @@
+import sys, os
+from ConfigParser import SafeConfigParser, RawConfigParser
+
+HAFARM_PATH = os.getenv("HAFARM_PATH", os.getenv("HAFARM_HOME",""))
+
+class Config(SafeConfigParser):
+    def __init__(self, *args, **kwargs):
+        ''' Read in configuration stored in multiply files
+            and locations.
+        '''
+        SafeConfigParser.__init__(self, *args, **kwargs)
+
+    def setup(self):
+        from glob import glob
+        paths = HAFARM_PATH.split(":")
+        for path in paths:
+            configs = glob(os.path.join(path, "*.cfg"))
+            self.read(configs)
 
 
-
-
-class Config(dict):
-	def __init__(self, *args, **kwargs):
-		''' Read in configuration stored in multiply files
-		    and locations.
-		'''
-		super(dict, self).__init__(*args, **kwargs)
