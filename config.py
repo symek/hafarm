fcb4774effe7403a3a4d92e52ac385e0c68ebcfb
Szymon Kapeniak <szymon.kapeniak@gmail.com>
2016-03-31 19:05:44 +0200

    Config based on ConfigParser. Not sure if fits the purpose. Hard to express python containers for example.

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import sys, os
 from ConfigParser import SafeConfigParser, RawConfigParser
+from collections import OrderedDict
 
 HAFARM_PATH = os.getenv("HAFARM_PATH", os.getenv("HAFARM_HOME",""))
 
@@ -14,7 +15,20 @@ class Config(SafeConfigParser):
         from glob import glob
         paths = HAFARM_PATH.split(":")
         for path in paths:
-            configs = glob(os.path.join(path, "*.cfg"))
+            configs = glob(os.path.join(path, "*.conf"))
             self.read(configs)
+
+    def __getitem__(self, section):
+        """ Returns a dictionary of options from a section.
+        """
+        if self.has_section(section):
+            conf = OrderedDict()
+            for option in self.options(section):
+                conf[option] = self.get(section, option)
+            return conf
+
+
+
+
 
 
