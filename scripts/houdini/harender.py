b109c19921d479f60af87b1252da3675ad72f48a
Szymon Kapeniak <szymon.kapeniak@gmail.com>
2016-08-10 19:17:04 +0200

    harender can edit export vex type on deep rasters.

--- scripts/houdini/harender.py
+++ scripts/houdini/harender.py
@@ -3,6 +3,9 @@ from optparse import OptionParser
 import sys, os
 from hafarm import utils
 
+# Global overwrites
+HAFARM_DISABLE_VECTORIZE_EXPORT = "HAFARM_DISABLE_VECTORIZE_EXPORT" in os.environ
+
 def help():
     return 'harender: Houdinis hrender csh script replacement.\
         \n\tusage: harender [-f "1 100" -i 1 -] -d /out/mantra1 myscene.hip \
@@ -20,22 +23,81 @@ def parseOptions():
     parser.add_option("", "--ignore_tiles", dest='ignore_tiles', action='store_true', default=False, help="Disables tiling on Mantra Rop (This allow custom ifd filtering setup).")
     parser.add_option("", "--generate_ifds", dest='generate_ifds', action='store_true', default=False, help="Changes Rop setting to save IFD files on disk. ")
     parser.add_option("", "--ifd_path", dest='ifd_path', action='store', default='$JOB/render/sungrid/ifd', help="Overwrites default IFD path.")
+    parser.add_option("", "--vectorize_export", dest='vectorize_export', action='store',  type="string", default="msk_*", help="Makes sure all deep rasters matching given pattern will be vector type.")
+    parser.add_option("", "--save_scene", dest='save_scene', action='store',  type="string", default="", help="Saves modified version of a scene mostly for debugging purposes.")
+    parser.add_option("", "--idle", dest='idle', action='store_true', default=False, help="Run the script, but don't render anything.")
     (opts, args) = parser.parse_args(sys.argv[1:])
     return opts, args
 
+
+def recursiveFindLockedParent(node):
+    """ Find a locked parent containing given node.
+    """
+    if not node.parent():
+        return None
+    if not node.isLockedHDA():
+        node = recursiveFindLockedParent(node.parent())
+    return node
+
+
+def vectorize_export(pattern, driver):
+    """ Makes sure given channels are vector type.
+    """
+    from fnmatch import fnmatch
+    binds_nodes = []
+    exports = []
+    variable_names = []
+    VECTOR_BIND_VOP_TYPE = 7
+
+    if driver.type().name() in ('ifd', "baketexture"):
+        exports += [parm for parm in driver.parms()\
+         if fnmatch(parm.evalAsString(), pattern)]
+
+    for plane in exports:
+        plane_name = plane.name().split("_")[-1]
+        vex_name   = "vm_vextype_" + plane_name
+        driver.parm(vex_name).set("vector")
+        variable_names += [plane.evalAsString()]
+
+    # Find export node inside Shaders:
+    shaders = hou.node("/obj").recursiveGlob("*", filter=hou.nodeTypeFilter.Shop)
+
+    for node in shaders:
+        # Filter all but binds nodes, with export name matching current ROP
+        # and active (exportparm set to 1 or 2)
+        binds = [n for n in node.allSubChildren() if n.type().name() == 'bind']
+        binds = [n for n in binds if n.parm("parmname").eval() in variable_names \
+            and n.parm("exportparm").eval() != 0]
+
+        # No matching exports? Go next shader. 
+        if not binds:
+            continue
+            
+        # Is it locked?
+        if node.isLockedHDA():
+            node.allowEditingOfContents()
+        # inside locked asset:
+        elif node.isInsideLockedHDA():
+            parent = recursiveFindLockedParent(node)
+            assert(parent)
+            parent.allowEditingOfContents()
+            
+        # Edit shaders:
+        for node in binds:
+            assert(not node.isInsideLockedHDA()) # This not should be the case, but just in case.
+            node.parm("parmtype").set(VECTOR_BIND_VOP_TYPE)
+            node.parmTuple("vectordef").set((1,1,1))
 
 
 def main():
     """Replacement for Houdini's own hrender script. Basic functions for rendering specified rop. 
        Main difference (reason to replace  hrender) was to allow rendring a list of randomly selected frames.
     """
-
     options, args     = parseOptions()
 
     if len(sys.argv) < 3:
         print help()
         sys.exit()
-
 
     # As we keep similarity to hredner, 
     # last argument of command line is a hip file. 
@@ -71,6 +133,20 @@ def main():
         ifd_name = os.path.join(options.ifd_path, scene_name + ".$F.ifd")
         driver.parm('soho_diskfile').set(ifd_name)
 
+    # vectorize exports:
+    if options.vectorize_export and not HAFARM_DISABLE_VECTORIZE_EXPORT:
+        vectorize_export(options.vectorize_export, driver)
+
+    if options.save_scene:
+        try:
+            hou.hipFile.save(file_name=options.save_scene)
+        except:
+            print "Error: Can't save a scene: ", 
+            print options.save_scene
+
+    if options.idle:
+        print "Warning: Idle mode, no rendering performed."
+        sys.exit()
 
     # Render with all details specified in a hip file:
     if not options.frame_list:
@@ -80,6 +156,8 @@ def main():
     else:
         for frame in utils.expand_sequence_into_digits(options.frame_list):
             driver.render(frame_range=(frame, frame), ignore_inputs=True, verbose=True)
+
+
         
 
 if __name__ == '__main__': main()
